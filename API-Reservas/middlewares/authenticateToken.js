8d87405fffa60c5a443ef737c54d8b84df83cdb3
Julio Bandeira Covary <147871168+juliocovary@users.noreply.github.com>
2024-12-20 15:33:32 -0300

    Update authenticateToken.js

--- API-Reservas/middlewares/authenticateToken.js
+++ API-Reservas/middlewares/authenticateToken.js
@@ -1,14 +1,33 @@
 import jwt from "jsonwebtoken";
 
-const secretKey = "seu-segredo-jwt";
-
 export const authenticateToken = (req, res, next) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Token não fornecido" });
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
-  jwt.verify(token, secretKey, (err, user) => {
-    if (err) return res.status(403).json({ message: "Token inválido" });
+  if (!token) {
+    return res.status(401).json({ message: 'Acesso negado' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Token inválido!' });
+    }
+
     req.user = user;
+    next();
+  });
+};
+
+export const admin = (req, res, next) => {
+  authenticateToken(req, res, (err) => {
+    if (err) {
+      return res.status(401).json({ message: 'Acesso negado' });
+    }
+
+    if (req.user.role !== 'admin') {
+      return res.status(403).json({ message: 'Acesso negado: privilégios insuficientes' });
+    }
+
     next();
   });
 };
